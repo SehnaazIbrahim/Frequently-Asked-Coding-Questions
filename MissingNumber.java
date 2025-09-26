0cb671145f02e715085118a1f4738904e47134f8
SehnaazIbrahim <159917934+SehnaazIbrahim@users.noreply.github.com>
2025-09-26 10:19:52 +0530

    Missing Number

---
+++ MissingNumber.java
@@ -0,0 +1,40 @@
+class Solution {
+
+  //TC: :O(N)  SC:O(1)
+    public int missingNumber(int[] arr) {
+        int xor = 0;
+        for (int i = 0; i <= arr.length; i++) {
+            xor = xor ^ i;
+        }
+      
+        for (int i = 0; i < arr.length; i++) {
+            xor = xor ^ arr[i];
+        }
+        return xor;
+
+    }
+
+
+  //  //TC: :O(N)  SC:O(N)
+  public int missingNumber(int[] arr) {
+        
+        int n= arr.length;
+        int[] hasharr= new int[n+1];
+
+        for(int i=0;i<arr.length;i++){
+                hasharr[arr[i]]++;
+        }
+
+        for(int i=0;i<hasharr.length;i++){
+            if(hasharr[i]==0){
+                return i;
+            }
+        }
+        return -1;
+
+
+
+    }
+
+  
+}
